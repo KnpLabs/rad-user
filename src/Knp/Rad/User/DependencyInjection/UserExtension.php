d4dc359ad62b3bed3c09d2c363cc3dfed13b4aac
David Jegat <david.jegat@knplabs.com>
2016-03-03 12:19:01 +0100

    Pass services into XML format

--- src/Knp/Rad/User/DependencyInjection/UserExtension.php
+++ src/Knp/Rad/User/DependencyInjection/UserExtension.php
@@ -6,7 +6,7 @@ use Knp\Rad\User\DependencyInjection\Configuration;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class UserExtension extends Extension
 {
@@ -15,8 +15,8 @@ class UserExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 
     /**
